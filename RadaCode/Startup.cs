c8830675465fcb36f2b5f8d23cc13b5c29c8ee72
Богдан Коломиец <bogdan_kolomiec@mail.ru>
2016-12-13 00:11:18 +0200

    Add project files.

---
+++ RadaCode/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RadaCode.Startup))]
+namespace RadaCode
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
